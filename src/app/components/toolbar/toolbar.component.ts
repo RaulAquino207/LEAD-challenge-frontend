bfdce42f32123a43b6d204867a688d1fcc06f0f8
Augusto_Cam95 <jose.augusto@dellead.com>
2021-07-01 14:40:18 -0300

    new routes and buttons

--- src/app/components/toolbar/toolbar.component.ts
+++ src/app/components/toolbar/toolbar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterContentChecked, Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
@@ -6,12 +6,24 @@ import { Router } from '@angular/router';
   templateUrl: './toolbar.component.html',
   styleUrls: ['./toolbar.component.scss']
 })
-export class ToolbarComponent implements OnInit {
+export class ToolbarComponent implements OnInit, AfterContentChecked {
+
+  currentRoute = '';
+  loginRoute = '/login';
+  homeRoute = '/home';
+  registrationRoute = '/registration';
+
 
   constructor(private router : Router) { }
 
 
   ngOnInit(): void {
+    this.currentRoute = this.router.url;
   }
+
+  ngAfterContentChecked() {
+    this.currentRoute = this.router.url;
+  }
+
 
 }
